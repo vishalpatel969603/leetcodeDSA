82993f83ce71e82ba80625ae47c7350c4964ff82
vishalpatel969603 <patelvishal89681@gmail.com>
2026-09-12 23:36:58 +0530

    Time: 3 ms (14.56%) | Memory: 24 MB (19.43%) - LeetSync

--- 283-move-zeroes/move-zeroes.cpp
+++ 283-move-zeroes/move-zeroes.cpp
@@ -5,13 +5,13 @@ public:
     int j=0;
     for(int i=0; i<n;i++ ){
         if(nums[i]!=0){
-            nums[j]=nums[i];
+           int v=nums[i];
+            nums[i]=0;
+             nums[j]=v;
              j++;
         }
-    }
-     while(j<n){
-        nums[j]=0;
-        j++;
+    
+     
      }     
     }
 };
